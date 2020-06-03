9b4496771647b98527c6b579b87b3777b386e47a
Ian Bertolacci <ian.bertolacci@gmail.com>
2020-06-03 12:03:16 -0700

    Adds Fixed Size, Strong Scaling ClayL tests
    Adds 3 ClayL tests with the following fixed sizes:
    + 100 x 100 x 240 (A size Michael tested, likely for his thesis)
    + 250 x 250 x 250 (A size michale tested, likely for his thesis)
    + 693 x 693 x 5   (a flatter comparison to 100x100x240; 1245 cells larger)

---
+++ benchmarks/clayl_250x250x240/assets/test_config.tcl
@@ -0,0 +1,22 @@
+# configuration file for Parflow test domain
+
+# set the number of runs that will be averaged
+set number_of_runs 1
+
+# set the directory where the parflow script will run from
+set test_run_dir ./
+
+# set the runname for this test domain
+set runname pf_clayl
+
+# define the Parflow run script file
+set scriptname clayl_fixed_size.tcl
+
+# mirror the test's output directory for Parflow outputs
+# output dir relative to test_run_dir
+set output_dir ./outputs
+
+# ClayL domain configuration
+set clayl_NX 250
+set clayl_NY 250
+set clayl_NZ 240
---
+++ benchmarks/clayl_250x250x240/assets/validate_results.tcl
@@ -0,0 +1,17 @@
+# Import the ParFlow TCL package
+lappend   auto_path $env(PARFLOW_DIR)/bin
+package   require parflow
+namespace import Parflow::*
+set runname $::env(runname)
+source ../pftest.tcl
+
+set sig_digits 9
+set py_test_epsilon 9
+
+set passed 1
+
+if $passed {
+   puts "$runname : PASSED"
+} {
+   puts "$runname : FAILED"
+}
